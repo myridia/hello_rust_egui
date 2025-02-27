c9cd9e71dfcf5a86583db0123940746e56771545
veto <veto@myridia.com>
2025-02-27 11:46:57 +0700

    update

--- README.md
+++ README.md
@@ -11,6 +11,11 @@ rustup target list
 
 
 ## Enable Zig 
+check the last zig https://ziglang.org/download/
 ```
+wget https://ziglang.org/builds/zig-linux-x86_64-0.14.0-dev.3385+055969b10.tar.xz
+tar -xvf zig*.tar.xz
+rm *.xz
+mv zig-* zig
 export PATH=$PATH:$(pwd)/zig
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![allow(rustdoc::missing_crate_level_docs)] // it's an example
+
 use eframe::egui;
-fn main() {
-    let options = eframe::NativeOptions::default();
+
+fn main() -> eframe::Result {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
     eframe::run_native(
         "My egui App",
         options,
-        Box::new(|_cc| Box::new(MyApp::default())),
-    );
+        Box::new(|cc| {
+            // This gives us image support:
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+
+            Ok(Box::<MyApp>::default())
+        }),
+    )
 }
 
 struct MyApp {
@@ -28,14 +40,19 @@ impl eframe::App for MyApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("My egui Application");
             ui.horizontal(|ui| {
-                ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name);
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
             });
             ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
+            if ui.button("Increment").clicked() {
                 self.age += 1;
             }
             ui.label(format!("Hello '{}', age {}", self.name, self.age));
+
+            ui.image(egui::include_image!(
+                "../../../crates/egui/assets/ferris.png"
+            ));
         });
     }
 }
